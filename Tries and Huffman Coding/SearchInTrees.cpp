76efcee3be399df30235eabb8852563894b64527
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2021-09-19 18:33:03 +0530

    Update SearchInTrees.cpp
    Updated code with latest solution

--- Tries and Huffman Coding/SearchInTrees.cpp
+++ Tries and Huffman Coding/SearchInTrees.cpp
@@ -1,99 +1,62 @@
-Code : Search in Tries
-Implement the function SearchWord for the Trie class.
-For a trie, write the function for searching a word. Return true if found successfully otherwise return false.
-Note : main function is given for your reference which we are using internally to test the code.
-
-	
-/*************************************************************** SOLUTION **********************************************************/
-	
-	
-#include <string>
-#include <vector>
-class TrieNode {
-	public :
-	char data;
-	TrieNode **children;
-	bool isTerminal;
-
-	TrieNode(char data) {
-		this -> data = data;
-		children = new TrieNode*[26];
-		for(int i = 0; i < 26; i++) {
-			children[i] = NULL;
-		}
-		isTerminal = true;          /////all node should be true becaz patter should be present middle in the string
-	}
+class TrieNode { 
+    public: char data;
+    TrieNode **children;
+    bool isTerminal;
+    TrieNode(char data) {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
 };
-/*      ////////////////check for it///////////
-6
-the moon shines brightly at night
-ig
-*/
 
 class Trie {
-	TrieNode *root;
-
-	public :
-	int count;
-
-	Trie() {
-		this->count = 0;
-		root = new TrieNode('\0');
-	}
-
-	bool insertWord(TrieNode *root, string word) {
-		// Base case
-		if(word.size() == 0) {
-			/*if (!root->isTerminal) {
-				root -> isTerminal = true;
-				return true;	
-			} else {
-				return false;
-			}*/
-            return true;
-		}
-
-		// Small Calculation
-		int index = word[0] - 'a';
-		TrieNode *child;
-		if(root -> children[index] != NULL) {
-			child = root -> children[index];
-		}
-		else {
-			child = new TrieNode(word[0]);
-			root -> children[index] = child;
-		}
-
-		// Recursive call
-		return insertWord(child, word.substr(1));
-	}
-
-	// For user
-	void insertWord(string word) {
-		if (insertWord(root, word)) {
-			this->count++;
-		}
-	}
-    bool search(TrieNode *root,string word)
-    {
-        if(word.size()==0 && root->isTerminal==true)
-            return true;
-        else if(word.size()==0 && root->isTerminal!=true)
+    TrieNode *root;
+    public: Trie() {
+        root = new TrieNode('\0');
+    }
+    void insertWord(TrieNode *root, string word) {
+        // Base case
+        if (word.size() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+        // Small calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        }
+        else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        // Recursive call 
+        insertWord(child, word.substr(1));
+    }
+    
+    void insertWord(string word) {
+        insertWord(root, word);
+    }
+    bool search(TrieNode *root, string word) {
+        if (word.size() == 0) { 
+            return root->isTerminal;
+        }
+        // Small calculation
+        int index = word[0] - 'a';
+        TrieNode *child; 
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        }
+        else {
             return false;
-        
-        int index=word[0]-'a';
-        
-        if(root->children[index]!=NULL)
-            return search(root->children[index],word.substr(1));
-        else
-            return false;
-        
-            
+        }
+        // Recursive call 
+        return search(child, word.substr(1));
     }
     bool search(string word) {
-        // Write your code here
-        return search(root,word);
-        
+        return search(root, word);
     }
-
 };
